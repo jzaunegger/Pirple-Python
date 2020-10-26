13c85413c1e4b3fce80bafb324465d2483bd5f8e
jzaunegger <jacksonzaunegger@gmail.com>
2020-10-26 16:48:09 -0400

    Adding Homework 1

---
+++ Homeworks/Homework-1/main.py
@@ -0,0 +1,29 @@
+"""
+Homework 1 for the Pirple Python-Is-Easy Course
+Author: jzaunegger
+Date: October 26th, 2020
+
+About: 
+    This homework was about learning about variables in Python3.
+    We are instructed in how to use them, but this is an active 
+    example designed to get us using them.
+"""
+
+
+# Declaring Variables
+SongName = "Medicated"
+Artist = "Wiz Khalifa"
+FeaturedArtists = "Chevy Woods, Juicy J"
+Album = "O.N.I.F.C"
+YearReleased = 2012
+Genre = "Hip Hop/Rap"
+SongDurationInMinutes = 5.5 
+
+# Printing Information
+print("Song Name:", SongName)
+print("Artist:", Artist)
+print("Featured Artists:", FeaturedArtists)
+print("Album Name:", Album)
+print("Year Released:", YearReleased)
+print("Musical Genre:", Genre)
+print("Song Duration in Minutes:", SongDurationInMinutes)
